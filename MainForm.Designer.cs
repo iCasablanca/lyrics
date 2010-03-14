facf1499eadda5eed25329d6c59c87d97d194feb
Rafael Steil <rafael@insnecorp.com>
2010-03-13 21:43:26 -0300

    Adding project's first version

---
+++ MainForm.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Lyrics
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.listITunesSongsButton = new System.Windows.Forms.Button();
+			this.searchLyricsButton = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.artistsListBox = new System.Windows.Forms.ListBox();
+			this.songsListBox = new System.Windows.Forms.ListBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.statusLabel = new System.Windows.Forms.Label();
+			this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+			this.SuspendLayout();
+			// 
+			// listITunesSongsButton
+			// 
+			this.listITunesSongsButton.Location = new System.Drawing.Point(12, 289);
+			this.listITunesSongsButton.Name = "listITunesSongsButton";
+			this.listITunesSongsButton.Size = new System.Drawing.Size(126, 23);
+			this.listITunesSongsButton.TabIndex = 1;
+			this.listITunesSongsButton.Text = "Listar músicas (itunes)";
+			this.listITunesSongsButton.UseVisualStyleBackColor = true;
+			this.listITunesSongsButton.Click += new System.EventHandler(this.listITunesSongsButton_Click);
+			// 
+			// searchLyricsButton
+			// 
+			this.searchLyricsButton.Location = new System.Drawing.Point(144, 289);
+			this.searchLyricsButton.Name = "searchLyricsButton";
+			this.searchLyricsButton.Size = new System.Drawing.Size(97, 23);
+			this.searchLyricsButton.TabIndex = 2;
+			this.searchLyricsButton.Text = "Procurar letras";
+			this.searchLyricsButton.UseVisualStyleBackColor = true;
+			this.searchLyricsButton.Click += new System.EventHandler(this.searchLyricsButton_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(41, 13);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "Artistas";
+			// 
+			// artistsListBox
+			// 
+			this.artistsListBox.FormattingEnabled = true;
+			this.artistsListBox.Location = new System.Drawing.Point(12, 25);
+			this.artistsListBox.Name = "artistsListBox";
+			this.artistsListBox.Size = new System.Drawing.Size(165, 238);
+			this.artistsListBox.TabIndex = 4;
+			this.artistsListBox.SelectedValueChanged += new System.EventHandler(this.artistsListBox_SelectedValueChanged);
+			// 
+			// songsListBox
+			// 
+			this.songsListBox.DisplayMember = "Name";
+			this.songsListBox.FormattingEnabled = true;
+			this.songsListBox.Location = new System.Drawing.Point(183, 25);
+			this.songsListBox.Name = "songsListBox";
+			this.songsListBox.Size = new System.Drawing.Size(252, 238);
+			this.songsListBox.TabIndex = 5;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(17, 270);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(43, 13);
+			this.label2.TabIndex = 6;
+			this.label2.Text = "Status: ";
+			// 
+			// statusLabel
+			// 
+			this.statusLabel.AutoSize = true;
+			this.statusLabel.Location = new System.Drawing.Point(67, 269);
+			this.statusLabel.Name = "statusLabel";
+			this.statusLabel.Size = new System.Drawing.Size(40, 13);
+			this.statusLabel.TabIndex = 7;
+			this.statusLabel.Text = "parado";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(512, 324);
+			this.Controls.Add(this.statusLabel);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.songsListBox);
+			this.Controls.Add(this.artistsListBox);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.searchLyricsButton);
+			this.Controls.Add(this.listITunesSongsButton);
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button listITunesSongsButton;
+		private System.Windows.Forms.Button searchLyricsButton;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.ListBox artistsListBox;
+		private System.Windows.Forms.ListBox songsListBox;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label statusLabel;
+		private System.ComponentModel.BackgroundWorker backgroundWorker;
+	}
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using iTunesLib;
+
+namespace Lyrics
+{
+	public partial class MainForm : Form
+	{
+		private Dictionary<string, List<TrackInformation>> artistsSongs = new Dictionary<string, List<TrackInformation>>();
+		private string BaseUrlFormat = "http://letras.terra.com.br/{0}";
+		private iTunesApp itunes;
+
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void listITunesSongsButton_Click(object sender, EventArgs e)
+		{
+			artistsListBox.Items.Clear();
+
+			itunes = new iTunesAppClass();
+			IITTrackCollection selectedTracks = itunes.SelectedTracks;
+
+			if (selectedTracks == null)
+			{
+				return;
+			}
+
+			OrganizeArtists(selectedTracks);
+			foreach (string artist in artistsSongs.Keys)
+			{
+				artistsListBox.Items.Add(artist);
+			}
+		}
+
+		private void OrganizeArtists(IITTrackCollection selectedTracks)
+		{
+			artistsSongs = new Dictionary<string, List<TrackInformation>>();
+			List<string> songs = new List<string>();
+
+			for (int i = 1; i <= selectedTracks.Count; i++)
+			{
+				IITFileOrCDTrack currentTrack = (IITFileOrCDTrack)selectedTracks[i];
+
+				if (!artistsSongs.ContainsKey(currentTrack.Artist))
+				{
+					artistsSongs.Add(currentTrack.Artist, new List<TrackInformation>());
+				}
+
+				artistsSongs[currentTrack.Artist].Add(new TrackInformation() { 
+					Track = currentTrack,
+					Index = i
+				});
+			}
+		}
+
+		private void searchLyricsButton_Click(object sender, EventArgs e)
+		{
+			if (artistsListBox.SelectedIndex > -1)
+			{
+				backgroundWorker = new BackgroundWorker();
+
+				backgroundWorker.DoWork += (bwSender, bwe) => {
+					ChangeCurrentStatus("Procurando artista e relação de letras...");
+					List<OnlineSongInformation> onlineSongs = ListOnlineSongs(SelectedArtist);
+
+					if (onlineSongs.Count == 0)
+					{
+						MessageBox.Show(this, String.Format("Nenhum resultado encontrado (artista: '{0}')",
+							NormalizeName(SelectedArtist)), "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Error);
+					}
+
+					foreach (OnlineSongInformation songInfo in onlineSongs)
+					{
+						IITFileOrCDTrack track = GetTrackFromListBox(songInfo.Name);
+
+						if (track != null)
+						{
+							ChangeCurrentStatus("Procurando letra para " + songInfo.Name);
+							string lyrics = GetSongLyrics(songInfo.Path);
+							int index = songsListBox.Items.IndexOf(songInfo.Name);
+
+							if (!String.IsNullOrEmpty(lyrics))
+							{
+								track.Lyrics = lyrics;
+								ChangeCurrentStatus("Encontrado letra para " + songInfo.Name);
+							}
+							else
+							{
+								ChangeCurrentStatus("Não encontrada letra para " + songInfo.Name);
+							}
+						}
+					}
+
+					ChangeCurrentStatus("Finalizado");
+				};
+
+				backgroundWorker.RunWorkerAsync();
+			}
+		}
+
+		private IITFileOrCDTrack GetTrackFromListBox(string onlineTrackName)
+		{
+			foreach (IITFileOrCDTrack track in songsListBox.Items)
+			{
+				if (track.Name.ToLower() == onlineTrackName)
+				{
+					return track;
+				}
+			}
+
+			return null;
+		}
+
+		private void ChangeCurrentStatus(string status)
+		{
+			if (statusLabel.InvokeRequired)
+			{
+				statusLabel.Invoke(new Action<string>(ChangeCurrentStatus), status);
+			}
+			else
+			{
+				statusLabel.Text = status;
+			}
+		}
+
+		private string SelectedArtist
+		{
+			get { return artistsListBox.SelectedItem.ToString(); }
+		}
+
+		private string GetSongLyrics(string path)
+		{
+			string lyrics = null;
+			string result = GetUrlContents(String.Format(BaseUrlFormat, path));
+
+			if (result != null && result.Contains("div_letra"))
+			{
+				string div = "<div id=\"div_letra\" >";
+				int index = result.IndexOf(div);
+				lyrics = result.Substring(index + div.Length, result.IndexOf("</div>", index) - (index + div.Length));
+			}
+
+			return lyrics;
+		}
+
+		private List<OnlineSongInformation> ListOnlineSongs(string artistName)
+		{
+			List<OnlineSongInformation> songs = new List<OnlineSongInformation>();
+			string result = GetUrlContents(String.Format(BaseUrlFormat, NormalizeName(artistName)));
+
+			if (result != null && result.Contains("identificador_artista"))
+			{
+				Match m = Regex.Match(result, "<a class=\"elemsug\" href=\"(.*)\">(.*)</a>");
+
+				while (m.Success)
+				{
+					songs.Add(new OnlineSongInformation() {
+						Path = m.Groups[1].Value,
+						Name = m.Groups[2].Value.ToLower()
+					});
+
+					m = m.NextMatch();
+				}
+			}
+			
+			return songs;
+		}
+
+		private string GetUrlContents(string url)
+		{
+			HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+			request.Method = "GET";
+			HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+			StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding("ISO-8859-1"));
+			string result = reader.ReadToEnd();
+			reader.Close();
+			return result;
+		}
+
+		private string NormalizeName(string name)
+		{
+			byte[] b = Encoding.GetEncoding(1251).GetBytes(name);
+			name = Encoding.ASCII.GetString(b).ToLower();
+			name = name.Replace(' ', '-')
+				.Replace("'", "")
+				.Replace('&', 'e')
+				.Replace('/', '-');
+			return name;
+		}
+
+		private void artistsListBox_SelectedValueChanged(object sender, EventArgs e)
+		{
+			if (artistsListBox.SelectedIndex > -1)
+			{
+				songsListBox.Items.Clear();
+				List<TrackInformation> songs = artistsSongs[SelectedArtist];
+
+				foreach (TrackInformation info in songs)
+				{
+					songsListBox.Items.Add(info.Track);
+				}
+			}
+		}
+	}
+
+	class TrackInformation
+	{
+		public IITFileOrCDTrack Track { get; set; }
+		public int Index { get; set; }
+
+		public override string ToString()
+		{
+			return Track.Name.ToLower();
+		}
+	}
+
+	class OnlineSongInformation
+	{
+		public string Name { get; set; }
+		public string Path { get; set; }
+	}
+}
